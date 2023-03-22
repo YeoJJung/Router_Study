e2ef79dc8c82890f9a168481fd8d3a71ea174e1a
YeoJJung <duwjdals1005@naver.com>
2023-03-23 00:02:26 +0900

    setting

---
+++ router_test/src/components/Nev.tsx
@@ -0,0 +1,11 @@
+export default function Nav(){
+    return(
+    <>
+    <h1>Nav</h1>
+    <ul className="navList">
+        <li><a href="">Home</a></li>
+        <li><a href="">About</a></li>
+    </ul>
+    </>
+    )
+}
---
+++ router_test/src/routes/About.tsx
@@ -0,0 +1,8 @@
+export default function About(){
+    return(
+    <>
+    <h1>About</h1>
+    <div>조교행님의 메인페이지에 온 걸 환영해</div>
+    </>
+    )
+}
